1bfd2ed98ee4c5132782f1cf6d1783e2a0b962eb
erg0nix <emil.jensen.balear+github@gmail.com>
2025-07-24 09:07:37 +0200

    feat(nvim): add telescope and cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -10,11 +10,9 @@ vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.wildmenu = true
 vim.opt.clipboard = "unnamedplus"
-
--- Completion options for LSP
 vim.o.completeopt = "menuone,noinsert,noselect"
 
--- Insert mode undo break mappings to create granular undo points
+-- Insert mode undo break mappings
 vim.cmd [[
   inoremap <Space> <Space><C-g>u
   inoremap . .<C-g>u
@@ -46,12 +44,10 @@ require("lazy").setup({
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        opts = {
-            transparent = true,
-        },
+        opts = { transparent = true },
     },
-    -- Statusline
     { "nvim-lualine/lualine.nvim" },
+
     -- Tree
     { "nvim-tree/nvim-web-devicons" },
     {
@@ -59,7 +55,7 @@ require("lazy").setup({
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             require("nvim-tree").setup({
-                filters = { 
+                filters = {
                     dotfiles = false,
                     custom = {},
                     exclude = { "scripts", ".env.*" },
@@ -74,11 +70,9 @@ require("lazy").setup({
                 },
                 on_attach = function(bufnr)
                     local api = require("nvim-tree.api")
-
                     local function opts(desc)
                         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
                     end
-
                     api.config.mappings.default_on_attach(bufnr)
                     vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
                     vim.keymap.set("n", "v", api.node.open.vertical, opts("Open in vertical split"))
@@ -90,77 +84,122 @@ require("lazy").setup({
         end,
     },
 
-    -- Treesitter for syntax highlighting
+    -- Telescope with FZF
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = vim.fn.executable("make") == 1,
+            },
+        },
+        config = function()
+            require("telescope").setup()
+            pcall(require("telescope").load_extension, "fzf")
+        end,
+    },
+
+    -- Treesitter
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-    -- LSP Support
+    -- LSP
     { "neovim/nvim-lspconfig" },
     { "williamboman/mason.nvim", config = true },
     { "williamboman/mason-lspconfig.nvim" },
     { "jay-babu/mason-null-ls.nvim" },
     { "jose-elias-alvarez/null-ls.nvim" },
 
-    -- Auto-completion
+    -- Completion
     { "hrsh7th/nvim-cmp" },
     { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-cmdline" },
+    { "onsails/lspkind.nvim" },
     { "L3MON4D3/LuaSnip" },
 
-    -- Linting & Formatting
+    -- Formatting / Linting
     { "stevearc/conform.nvim" },
     { "mfussenegger/nvim-lint" },
 
+    -- Better diagnostics UI
+    { "folke/trouble.nvim", opts = {} },
+
+    -- Debugging
+    { "mfussenegger/nvim-dap" },
+    { "rcarriga/nvim-dap-ui" },
+    { "nvim-neotest/nvim-nio" }, -- Required by dap-ui
     {
-        'mrcjkb/rustaceanvim',
-        version = '^5',
-        lazy = false,
+        "leoluz/nvim-dap-go",
+        config = function()
+            require("dap-go").setup()
+        end
     },
-    { 'rust-lang/rust.vim' },
+
+    -- Rust
+    { "mrcjkb/rustaceanvim", version = "^5", lazy = false },
+    { "rust-lang/rust.vim" },
 })
 
+-- Theme and transparent tree
 vim.cmd[[colorscheme tokyonight-night]]
 vim.cmd([[ highlight NvimTreeNormal guibg=NONE ctermbg=NONE ]])
 vim.cmd([[ highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE ]])
 vim.cmd([[ highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE ]])
 
--- Ensure keymaps load after Lazy.nvim initializes
+-- Lazy-load keymaps
 vim.api.nvim_create_autocmd("User", {
     pattern = "LazyLoad",
     callback = function()
-        vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>xx', "<cmd>TroubleToggle<cr>", { noremap = true, silent = true })
     end,
 })
 
--- Treesitter configuration
+-- Treesitter
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "lua", "python", "rust", "javascript", "tsx", "json", "go" },
   highlight = { enable = true },
   indent = { enable = true },
 })
 
--- Mason & LSP configuration
+-- Mason + LSP
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "pyright", "ts_ls", "jsonls", "gopls" },
+  ensure_installed = { "lua_ls", "pyright", "ts_ls", "jsonls", "gopls" }, -- ts_ls instead of deprecated tsserver
   automatic_installation = true,
 })
 
+-- LSP servers
 local lspconfig = require("lspconfig")
 local servers = {
   lua_ls = {},
   pyright = {},
-  ts_ls = {},
+  ts_ls = {}, -- use ts_ls here too
   jsonls = {},
-  gopls = {},
+  gopls = {
+    settings = {
+      gopls = {
+        analyses = {
+          unusedparams = true,
+          shadow = true,
+        },
+        staticcheck = true,
+      },
+    },
+  },
 }
-
-for server, config in pairs(servers) do
-  lspconfig[server].setup(config)
+for name, config in pairs(servers) do
+  lspconfig[name].setup(config)
 end
 
+-- LSP UI
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "●", -- or '■', '▎', etc.
-  },
+  virtual_text = { prefix = "●" },
   signs = true,
   underline = true,
   update_in_insert = false,
@@ -171,14 +210,30 @@ vim.keymap.set('n', '<leader>e', function()
   vim.diagnostic.open_float(nil, { focusable = true, border = "rounded" })
 end, { noremap = true, silent = true })
 
-
-
--- Map Ctrl+B to jump to definition
 vim.keymap.set('n', '<C-b>', vim.lsp.buf.definition, { noremap = true, silent = true })
-
--- Map Ctrl+K to show hover documentation
 vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, { noremap = true, silent = true })
 
+-- Completion
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+cmp.setup({
+  formatting = {
+    format = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 }),
+  },
+  snippet = {
+    expand = function(args) require("luasnip").lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "buffer" },
+  }),
+})
 
 -- Linting
 require("lint").linters_by_ft = {
@@ -191,9 +246,7 @@ require("lint").linters_by_ft = {
   go = { "golangcilint" },
 }
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+  callback = function() require("lint").try_lint() end,
 })
 
 -- Formatting
@@ -210,4 +263,23 @@ require("conform").setup({
   format_on_save = true,
 })
 
+-- DAP UI (safe load)
+local dap_status, dapui = pcall(require, "dapui")
+if dap_status then
+  local dap = require("dap")
+  dapui.setup()
+  dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+  dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+  dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+end
+
+-- DAP keymaps
+vim.keymap.set("n", "<F5>", function() require("dap").continue() end)
+vim.keymap.set("n", "<F10>", function() require("dap").step_over() end)
+vim.keymap.set("n", "<F11>", function() require("dap").step_into() end)
+vim.keymap.set("n", "<F12>", function() require("dap").step_out() end)
+vim.keymap.set("n", "<Leader>b", function() require("dap").toggle_breakpoint() end)
+
+-- Statusline
 require("lualine").setup()
+
